20e930681d99ef01ba7ce02d85995f31b3e096ac
EthanN <newberrydethan@gmail.com>
2022-02-24 21:58:37 -0600

    CAC 102-2 Lesson 2 My Application Overview

---
+++ lessons/lesson2.py
@@ -0,0 +1,66 @@
+# My Application Overview Lesson
+
+# This function should be lowercase and if more than one word it should have
+# an underscore to separate each word. You should have a paranthesis after
+# the name followed by a colon. Statements that are part of a function should
+# be 4 spaces indented
+
+def my_function():
+    print('Hello')
+    print('World')
+
+
+# To run the function, you must call it by name.
+my_function()
+
+
+# When defining a function with argyments, those argynebts go in between the
+# paranthesis and separated by commas
+
+def my_other(arg1, arg2):
+    print(arg1)
+    print(arg2)
+
+
+my_other("Red", "Green")
+
+
+# Variable Names
+# - must start with a letter or and underscore
+# - Can not begin with a number
+# - Can only contain alpha-numeric character and underscores
+# - Are case sensitive
+
+# The variables are on the left while the literal value is on the right
+value = 'Blue'
+value2 = 10
+
+# Variables can even change types
+value3 = 'Happy'
+value3 = 20
+
+
+# Multi-line statements use a backslash to continue a statement
+alpha = 1 + 2 + 3 \
+    + 4 + 5
+
+# more than one variable declared
+beta = 10; hello = "his"; delta = 5
+
+up, down, left = 'town', 'ok', 'right'
+
+
+# A class is an object, which is a blueprint. An instantiation of an object
+# allows for all fields and functions to be accessed within. Like functions
+# outside the class, you need to provide two empty spaces before it is defined.
+
+class MyFirstClass:
+    name = 'Ethan'
+
+    def something(self):
+        print('Something')
+
+
+my_class = MyFirstClass()
+print(my_class.name)
+my_class.something()
