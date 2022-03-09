3d9859e041fa20271263f2c2ee0323d512d1ccf7
EthanN <newberrydethan@gmail.com>
2022-03-08 22:35:37 -0600

    CAC-102-13 Modules & Packages

---
+++ lessons/lesson13.py
@@ -0,0 +1,19 @@
+# Lesson 13 - Modules and Packages
+# import happy
+
+# print(happy.express())
+
+import happy as hey
+from math import pi
+import house
+
+print(hey.express(False))
+
+print('The value for pi is {}'.format(pi))
+
+house3 = house.House(27, 'wood', 'bamboo', 'green')
+print(house3.door_color)
+
+if __name__ == '__main__':
+    house2 = house.House(21, 'dirt', 'straw', 'blue')
+    house2.door_open_close()
